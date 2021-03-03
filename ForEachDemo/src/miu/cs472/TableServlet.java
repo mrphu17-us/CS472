6eb7582c5437a169eb63dfd8522ec6e21de45ff7
John <mrphu17@gmail.com>
2021-03-02 21:29:24 -0600

    quiz lab upgrade

---
+++ ForEachDemo/src/miu/cs472/TableServlet.java
@@ -0,0 +1,41 @@
+package miu.cs472;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class TableServlet
+ */
+@WebServlet("/TableServlet")
+public class TableServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		Student[] table = new Student[] { new Student("bob", 23), new Student("jill", 33), new Student("kim", 18) };
+		request.setAttribute("students", table);
+		RequestDispatcher dispatcher = request.getRequestDispatcher("table.jsp");
+		dispatcher.forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		RequestDispatcher dispatcher = request.getRequestDispatcher("table.jsp");
+		dispatcher.forward(request, response);
+	}
+
+}
