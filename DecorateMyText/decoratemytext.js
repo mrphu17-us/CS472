a04997f26d40b0599e03d3cfd2eb4d9412490218
John <mrphu17@gmail.com>
2021-03-09 13:52:25 -0600

    lab js

--- DecorateMyText/decoratemytext.js
+++ DecorateMyText/decoratemytext.js
@@ -24,6 +24,7 @@ function checkBoxChanged() {
         element.style.fontWeight = "";
         element.style.color = "";
         element.style.textDecoration = "";
+        document.body.style.backgroundImage = "";
     }
 }
 
